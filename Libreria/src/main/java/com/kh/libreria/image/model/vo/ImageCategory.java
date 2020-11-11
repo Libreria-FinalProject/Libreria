18a593d54a1544e11eee921a0c1037ad4cdb4146
HoYoungChoi122 <66100594+HoYoungChoi122@users.noreply.github.com>
2020-11-11 11:34:47 +0900

    Update ImageCategory.java

--- Libreria/src/main/java/com/kh/libreria/image/model/vo/ImageCategory.java
+++ Libreria/src/main/java/com/kh/libreria/image/model/vo/ImageCategory.java
@@ -1,5 +1,39 @@
 package com.kh.libreria.image.model.vo;
 
 public class ImageCategory {
+	private int ic_no;
+	private String ic_name;
+	
+	public ImageCategory() {
+		
+	}
 
+	public ImageCategory(int ic_no, String ic_name) {
+		super();
+		this.ic_no = ic_no;
+		this.ic_name = ic_name;
+	}
+
+	public int getIc_no() {
+		return ic_no;
+	}
+
+	public void setIc_no(int ic_no) {
+		this.ic_no = ic_no;
+	}
+
+	public String getIc_name() {
+		return ic_name;
+	}
+
+	public void setIc_name(String ic_name) {
+		this.ic_name = ic_name;
+	}
+
+	@Override
+	public String toString() {
+		return "ImageCategory [ic_no=" + ic_no + ", ic_name=" + ic_name + "]";
+	}
+
+	
 }
